ae2b26fa26008a96c82174468d747dd54945b2c0
Miguel Galvez <m.galvez.dev@gmail.com>
2022-11-09 12:22:29 -0800

    stored list of probabilities

--- naive_bayes_adult.py
+++ naive_bayes_adult.py
@@ -120,62 +120,76 @@ def naive_bayes(ad_df, test_df, LABELS):
                 a_cnt_n,f_cnt_n,e_cnt_n,m_cnt_n,o_cnt_n,\
                 r_cnt_n,rc_cnt_n,sx_cnt_n,edn_cnt_n,\
                 edn_cnt_n,cpgain_cnt_n,cploss_cnt_n,hrs_cnt_n,nc_cnt_n
-                
-    # Parse data from pandas dataframes into parallel lists
-    for label in LABELS: globals()[label] = parse(label, ad_df)
-    for label in LABELS: globals()[f"test_{label}"] = parse(label, test_df)
-    
-    # Discretize labels
-    disc_age=discretize(age, 40, None) # 0 for <=40 1 for >40
-    disc_fnlwgt=discretize(fnlwgt, 100000, None) # 0 for <=100000 1 for >100000
-    disc_education=discretize(education,None,' Bachelors') # Bachelors (1) versus anything else(0)
-    disc_marital_status=discretize(marital_status,None,' Divorced') # Divorced (1) versus anything else(0)
-    disc_occupation=discretize(occupation,None,' Other-service') # Other service (1) versus anything else(0)
-    disc_relationship=discretize(relationship,None,' Unmarried') # Never worked (1) versus anything else(0)
-    disc_race=discretize(race,None,' White') # White (1) versus any other race(0)
-    disc_sex=discretize(sex,None,' Male') # Male (1) versus Female(0)
-    disc_education_num=discretize(education_num, 40, None) # 0 for <=40 1 for >40
-    disc_capital_gain=discretize(capital_gain, 0, None) # any capital gain (1) or none (0)
-    disc_capital_loss=discretize(capital_loss, 0, None) # any capital loss (1) or none (0)
-    disc_hours_per_week=discretize(hours_per_week, 39, None)  # full time or part time
-    disc_native_country=discretize(native_country,None,' United-States') # 0 if not from US, 1 if from US
-    disc_50k=discretize(_50k,None,' >50K') # 1 for greater than 50k 0 for lower than 50k  
-    # Discretize test labels
-    test_disc_age=discretize(test_age, 40, None) # 0 for <=40 1 for >40
-    test_disc_fnlwgt=discretize(test_fnlwgt, 100000, None) # 0 for <=100000 1 for >100000
-    test_disc_education=discretize(test_education,None,' Bachelors') # Bachelors (1) versus anything else(0)
-    test_disc_marital_status=discretize(test_marital_status,None,' Divorced') # Divorced (1) versus anything else(0)
-    test_disc_occupation=discretize(test_occupation,None,' Other-service') # Other service (1) versus anything else(0)
-    test_disc_relationship=discretize(test_relationship,None,' Unmarried') # Never worked (1) versus anything else(0)
-    test_disc_race=discretize(test_race,None,' White') # White (1) versus any other race(0)
-    test_disc_sex=discretize(test_sex,None,' Male') # Male (1) versus Female(0)
-    test_disc_education_num=discretize(test_education_num, 40, None) # 0 for <=40 1 for >40
-    test_disc_capital_gain=discretize(test_capital_gain, 0, None) # any capital gain (1) or none (0)
-    test_disc_capital_loss=discretize(test_capital_loss, 0, None) # any capital loss (1) or none (0)
-    test_disc_hours_per_week=discretize(test_hours_per_week, 39, None)  # full time or part time
-    test_disc_native_country=discretize(test_native_country,None,' United-States') # 0 if not from US, 1 if from US
-    test_disc_50k=discretize(test__50k,None,' >50K') # 1 for greater than 50k 0 for lower than 50k  
-    
-    # Generate all probabilities for each label (X)
-    
-    # P(X=1|>50k) and P(X=1|<=50k)
-    a_cnt,f_cnt,e_cnt,m_cnt,o_cnt,r_cnt,rc_cnt,sx_cnt,edn_cnt,edn_cnt,cpgain_cnt,\
-    cploss_cnt,hrs_cnt,nc_cnt,a_cnt_n,f_cnt_n,e_cnt_n,m_cnt_n,o_cnt_n,r_cnt_n,rc_cnt_n,sx_cnt_n,\
-    edn_cnt_n,edn_cnt_n,cpgain_cnt_n,cploss_cnt_n,hrs_cnt_n,nc_cnt_n\
-    =prob(disc_age,disc_fnlwgt,disc_education,disc_marital_status,disc_occupation,
-          disc_relationship,disc_race,disc_sex,disc_education_num,
-          disc_capital_gain, disc_capital_loss, disc_hours_per_week, 
-          disc_native_country, disc_50k, True)
-    # <=50k P(X=0|>50k) and P(X=0|<=50k)
-    a_cnt_0,f_cnt_0,e_cnt_0,m_cnt_0,o_cnt_0,r_cnt_0,rc_cnt_0,sx_cnt_0,edn_cnt_0,edn_cnt_0,cpgain_cnt_0,\
-    cploss_cnt_0,hrs_cnt_0,nc_cnt_0,a_cnt_n_0,f_cnt_n_0,e_cnt_n_0,m_cnt_n_0,o_cnt_n_0,\
-    r_cnt_n_0,rc_cnt_n_0,sx_cnt_n_0,edn_cnt_n_0,edn_cnt_n_0,cpgain_cnt_n_0,\
-    cploss_cnt_n_0,hrs_cnt_n_0,nc_cnt_n_0\
-    =prob(disc_age,disc_fnlwgt,disc_education,disc_marital_status,
-          disc_occupation,disc_relationship,disc_race,disc_sex,disc_education_num,
-          disc_capital_gain, disc_capital_loss, disc_hours_per_week, disc_native_country, disc_50k, False)
-    
-
+    def gen_list_prob(LABELS,ad_df):
+        # Parse data from pandas dataframes into parallel lists
+        for label in LABELS: globals()[label] = parse(label, ad_df)
+        # Discretize labels
+        disc_age=discretize(age, 40, None) # 0 for <=40 1 for >40
+        disc_fnlwgt=discretize(fnlwgt, 100000, None) # 0 for <=100000 1 for >100000
+        disc_education=discretize(education,None,' Bachelors') # Bachelors (1) versus anything else(0)
+        disc_marital_status=discretize(marital_status,None,' Divorced') # Divorced (1) versus anything else(0)
+        disc_occupation=discretize(occupation,None,' Other-service') # Other service (1) versus anything else(0)
+        disc_relationship=discretize(relationship,None,' Unmarried') # Never worked (1) versus anything else(0)
+        disc_race=discretize(race,None,' White') # White (1) versus any other race(0)
+        disc_sex=discretize(sex,None,' Male') # Male (1) versus Female(0)
+        disc_education_num=discretize(education_num, 40, None) # 0 for <=40 1 for >40
+        disc_capital_gain=discretize(capital_gain, 0, None) # any capital gain (1) or none (0)
+        disc_capital_loss=discretize(capital_loss, 0, None) # any capital loss (1) or none (0)
+        disc_hours_per_week=discretize(hours_per_week, 39, None)  # full time or part time
+        disc_native_country=discretize(native_country,None,' United-States') # 0 if not from US, 1 if from US
+        disc_50k=discretize(_50k,None,' >50K') # 1 for greater than 50k 0 for lower than 50k  
+        # Generate all probabilities for each label (X)
+        # P(X=1|>50k) and P(X=1|<=50k)
+        a_cnt,f_cnt,e_cnt,m_cnt,o_cnt,r_cnt,rc_cnt,sx_cnt,edn_cnt,edn_cnt,cpgain_cnt,\
+        cploss_cnt,hrs_cnt,nc_cnt,a_cnt_n,f_cnt_n,e_cnt_n,m_cnt_n,o_cnt_n,r_cnt_n,rc_cnt_n,sx_cnt_n,\
+        edn_cnt_n,edn_cnt_n,cpgain_cnt_n,cploss_cnt_n,hrs_cnt_n,nc_cnt_n\
+        =prob(disc_age,disc_fnlwgt,disc_education,disc_marital_status,disc_occupation,
+            disc_relationship,disc_race,disc_sex,disc_education_num,
+            disc_capital_gain, disc_capital_loss, disc_hours_per_week, 
+            disc_native_country, disc_50k, True)
+        # <=50k P(X=0|>50k) and P(X=0|<=50k)
+        a_cnt_0,f_cnt_0,e_cnt_0,m_cnt_0,o_cnt_0,r_cnt_0,rc_cnt_0,sx_cnt_0,edn_cnt_0,edn_cnt_0,cpgain_cnt_0,\
+        cploss_cnt_0,hrs_cnt_0,nc_cnt_0,a_cnt_n_0,f_cnt_n_0,e_cnt_n_0,m_cnt_n_0,o_cnt_n_0,\
+        r_cnt_n_0,rc_cnt_n_0,sx_cnt_n_0,edn_cnt_n_0,edn_cnt_n_0,cpgain_cnt_n_0,\
+        cploss_cnt_n_0,hrs_cnt_n_0,nc_cnt_n_0\
+        =prob(disc_age,disc_fnlwgt,disc_education,disc_marital_status,
+            disc_occupation,disc_relationship,disc_race,disc_sex,disc_education_num,
+            disc_capital_gain, disc_capital_loss, disc_hours_per_week, disc_native_country, disc_50k, False)
+        # Store probabilities in appropriate lists
+        LIST_P_GT50K_1=[a_cnt,f_cnt,e_cnt,m_cnt,o_cnt,r_cnt,rc_cnt,sx_cnt,edn_cnt,edn_cnt,cpgain_cnt,
+                        cploss_cnt,hrs_cnt,nc_cnt]
+        LIST_P_LT50K_1=[a_cnt_n,f_cnt_n,e_cnt_n,m_cnt_n,o_cnt_n,r_cnt_n,rc_cnt_n,sx_cnt_n,
+                        edn_cnt_n,edn_cnt_n,cpgain_cnt_n,cploss_cnt_n,hrs_cnt_n,nc_cnt_n]
+        LIST_P_GT50K_0=[a_cnt_0,f_cnt_0,e_cnt_0,m_cnt_0,o_cnt_0,r_cnt_0,rc_cnt_0,sx_cnt_0,edn_cnt_0,
+                        edn_cnt_0,cpgain_cnt_0,cploss_cnt_0,hrs_cnt_0,nc_cnt_0]
+        LIST_P_LT50K_0=[a_cnt_n_0,f_cnt_n_0,e_cnt_n_0,m_cnt_n_0,o_cnt_n_0,
+                        r_cnt_n_0,rc_cnt_n_0,sx_cnt_n_0,edn_cnt_n_0,edn_cnt_n_0,cpgain_cnt_n_0,
+                        cploss_cnt_n_0,hrs_cnt_n_0,nc_cnt_n_0]
+        return LIST_P_GT50K_1,LIST_P_LT50K_1,LIST_P_GT50K_0,LIST_P_LT50K_0
+    def test(LABELS,test_df): #Classifies >50k or <=50k over all test instances
+        for label in LABELS: globals()[f"test_{label}"] = parse(label, test_df)
+        # Discretize test labels
+        test_disc_age=discretize(test_age, 40, None) # 0 for <=40 1 for >40
+        test_disc_fnlwgt=discretize(test_fnlwgt, 100000, None) # 0 for <=100000 1 for >100000
+        test_disc_education=discretize(test_education,None,' Bachelors') # Bachelors (1) versus anything else(0)
+        test_disc_marital_status=discretize(test_marital_status,None,' Divorced') # Divorced (1) versus anything else(0)
+        test_disc_occupation=discretize(test_occupation,None,' Other-service') # Other service (1) versus anything else(0)
+        test_disc_relationship=discretize(test_relationship,None,' Unmarried') # Never worked (1) versus anything else(0)
+        test_disc_race=discretize(test_race,None,' White') # White (1) versus any other race(0)
+        test_disc_sex=discretize(test_sex,None,' Male') # Male (1) versus Female(0)
+        test_disc_education_num=discretize(test_education_num, 40, None) # 0 for <=40 1 for >40
+        test_disc_capital_gain=discretize(test_capital_gain, 0, None) # any capital gain (1) or none (0)
+        test_disc_capital_loss=discretize(test_capital_loss, 0, None) # any capital loss (1) or none (0)
+        test_disc_hours_per_week=discretize(test_hours_per_week, 39, None)  # full time or part time
+        test_disc_native_country=discretize(test_native_country,None,' United-States') # 0 if not from US, 1 if from US
+        test_disc_50k=discretize(test__50k,None,' >50K') # 1 for greater than 50k 0 for lower than 50k  
+        return None
+    def test_label(test_label,LIST_P): # Search function that returns needed probabilities from list
+        # P(>50k|test_label)
+        return None  
+    def error():
+        return None
+    LIST_P_GT50K_1,LIST_P_LT50K_1,LIST_P_GT50K_0,LIST_P_LT50K_0=gen_list_prob(LABELS,ad_df)
 # Program begins here
 
 # Names of files we will read from
