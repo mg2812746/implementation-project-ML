308a9aa8b1c74cf4c77179293dc80b7aa6aee505
Miguel Galvez <m.galvez.dev@gmail.com>
2022-11-09 14:53:09 -0800

    finished

--- naive_bayes_adult.py
+++ naive_bayes_adult.py
@@ -120,7 +120,7 @@ def naive_bayes(ad_df, test_df, LABELS):
                 a_cnt_n,f_cnt_n,e_cnt_n,m_cnt_n,o_cnt_n,\
                 r_cnt_n,rc_cnt_n,sx_cnt_n,edn_cnt_n,\
                 edn_cnt_n,cpgain_cnt_n,cploss_cnt_n,hrs_cnt_n,nc_cnt_n
-    def gen_list_prob(LABELS,ad_df):
+    def gen_list_prob(LABELS,ad_df): #Creates a list of probabilities
         # Parse data from pandas dataframes into parallel lists
         for label in LABELS: globals()[label] = parse(label, ad_df)
         # Discretize labels
@@ -166,7 +166,15 @@ def naive_bayes(ad_df, test_df, LABELS):
                         r_cnt_n_0,rc_cnt_n_0,sx_cnt_n_0,edn_cnt_n_0,edn_cnt_n_0,cpgain_cnt_n_0,
                         cploss_cnt_n_0,hrs_cnt_n_0,nc_cnt_n_0]
         return LIST_P_GT50K_1,LIST_P_LT50K_1,LIST_P_GT50K_0,LIST_P_LT50K_0
-    def test(LABELS,test_df): #Classifies >50k or <=50k over all test instances
+    def prob_from_list(LIST_P,index): # Search function that returns needed probabilities from list
+        """   INDICES ARE AS FOLLOWS:
+                0=age, 1=fnlwgt, 2=education, 3=education_num, 4=marital-status, 5=occupation
+                6=relationship, 7=race , 8=sex, 9=capital-gain, 10=capital-loss, 11=hours-per-week, 12=native-country
+        """
+        return LIST_P[index]  
+    def test(LABELS,test_df,P_GT50K_1,P_LT50K_1,P_GT50K_0,P_LT50K_0): #Returns a list of all classified test instances for >50k or <=50k
+        
+        classified=[]
         for label in LABELS: globals()[f"test_{label}"] = parse(label, test_df)
         # Discretize test labels
         test_disc_age=discretize(test_age, 40, None) # 0 for <=40 1 for >40
@@ -182,26 +190,106 @@ def naive_bayes(ad_df, test_df, LABELS):
         test_disc_capital_loss=discretize(test_capital_loss, 0, None) # any capital loss (1) or none (0)
         test_disc_hours_per_week=discretize(test_hours_per_week, 39, None)  # full time or part time
         test_disc_native_country=discretize(test_native_country,None,' United-States') # 0 if not from US, 1 if from US
-        test_disc_50k=discretize(test__50k,None,' >50K') # 1 for greater than 50k 0 for lower than 50k  
-        return None
-    def test_label(test_label,LIST_P): # Search function that returns needed probabilities from list
-        # P(>50k|test_label)
-        return None  
-    def error():
-        return None
-    LIST_P_GT50K_1,LIST_P_LT50K_1,LIST_P_GT50K_0,LIST_P_LT50K_0=gen_list_prob(LABELS,ad_df)
+        test_disc_50k=discretize(test__50k,None,' >50K') # THESE WILL BE USED TO COMPARE COMPUTED CLASSIFICATION VS ACTUAL 
+        # Traverse through all test instances and classify each one, which is then stored into a list and then return that list
+        for a,f,e,m,o,r,rc,sx,edn,cpgain,cploss,hrs,nc in zip(test_disc_age,test_disc_fnlwgt,
+                                                                  test_disc_education,test_disc_marital_status,test_disc_occupation,
+                                                                  test_disc_relationship,test_disc_race,test_disc_sex,test_disc_education_num,
+                                                                  test_disc_capital_gain,test_disc_capital_loss,
+                                                                  test_disc_hours_per_week,test_disc_native_country):
+            prob_yes=[i for i in range(13)]
+            prob_no=[i for i in range(13)]
+            # assign probability of >50k income (yes) and <=50k income (no) given age>40 to list
+            if a == 1 :  prob_yes[0]=prob_from_list(P_GT50K_1,0); prob_no[0]=prob_from_list(P_LT50K_1,0)
+            else: prob_yes[0]=prob_from_list(P_GT50K_0,0); prob_no[0]=prob_from_list(P_LT50K_0,0)      
+            if f == 1 : prob_yes[1]=prob_from_list(P_GT50K_1,1); prob_no[1]=prob_from_list(P_LT50K_1,1)
+            else: prob_yes[1]=prob_from_list(P_GT50K_0,1); prob_no[1]=prob_from_list(P_LT50K_0,1)
+            if e == 1 : prob_yes[2]=prob_from_list(P_GT50K_1,2); prob_no[2]=prob_from_list(P_LT50K_1,2)
+            else: prob_yes[2]=prob_from_list(P_GT50K_0,2); prob_no[2]=prob_from_list(P_LT50K_0,2)
+            if m == 1 : prob_yes[3]=prob_from_list(P_GT50K_1,3); prob_no[3]=prob_from_list(P_LT50K_1,3)
+            else: prob_yes[3]=prob_from_list(P_GT50K_0,3); prob_no[3]=prob_from_list(P_LT50K_0,3)
+            if o == 1 : prob_yes[4]=prob_from_list(P_GT50K_1,4); prob_no[4]=prob_from_list(P_LT50K_1,4)
+            else: prob_yes[4]=prob_from_list(P_GT50K_0,4); prob_no[4]=prob_from_list(P_LT50K_0,4)
+            if r == 1 : prob_yes[5]=prob_from_list(P_GT50K_1,5); prob_no[5]=prob_from_list(P_LT50K_1,5)
+            else: prob_yes[5]=prob_from_list(P_GT50K_0,5); prob_no[5]=prob_from_list(P_LT50K_0,5)
+            if rc == 1 :prob_yes[6]=prob_from_list(P_GT50K_1,6); prob_no[6]=prob_from_list(P_LT50K_1,6)
+            else: prob_yes[6]=prob_from_list(P_GT50K_0,6); prob_no[6]=prob_from_list(P_LT50K_0,6)
+            if sx == 1 : prob_yes[7]=prob_from_list(P_GT50K_1,7); prob_no[7]=prob_from_list(P_LT50K_1,7)
+            else: prob_yes[7]=prob_from_list(P_GT50K_0,7); prob_no[7]=prob_from_list(P_LT50K_0,7)
+            if edn == 1 : prob_yes[8]=prob_from_list(P_GT50K_1,8); prob_no[8]=prob_from_list(P_LT50K_1,8)
+            else: prob_yes[8]=prob_from_list(P_GT50K_0,8); prob_no[8]=prob_from_list(P_LT50K_0,8)
+            if cpgain == 1 : prob_yes[9]=prob_from_list(P_GT50K_1,9); prob_no[9]=prob_from_list(P_LT50K_1,9)
+            else: prob_yes[9]=prob_from_list(P_GT50K_0,9); prob_no[9]=prob_from_list(P_LT50K_0,9)
+            if cploss == 1 : prob_yes[10]=prob_from_list(P_GT50K_1,10); prob_no[10]=prob_from_list(P_LT50K_1,10)
+            else: prob_yes[10]=prob_from_list(P_GT50K_0,10); prob_no[10]=prob_from_list(P_LT50K_0,10)
+            if hrs == 1 : prob_yes[11]=prob_from_list(P_GT50K_1,11); prob_no[11]=prob_from_list(P_LT50K_1,11)
+            else: prob_yes[11]=prob_from_list(P_GT50K_0,11); prob_no[11]=prob_from_list(P_LT50K_0,11)
+            if nc == 1 : prob_yes[12]=prob_from_list(P_GT50K_1,12); prob_no[12]=prob_from_list(P_LT50K_1,12)
+            else: prob_yes[12]=prob_from_list(P_GT50K_0,12); prob_no[12]=prob_from_list(P_LT50K_0,12)
+            # After we get our probabilities, we compare the probability of yes vs no overall 
+            # Then classify based on which probability is greater
+            prob_yes_fin=0
+            prob_no_fin=0
+            avoid_yes=0
+            avoid_no=0
+            for i in range(0,len(prob_yes)):
+                if(prob_yes[i]!=0):
+                    prob_yes_fin=prob_yes[i]
+                    avoid_yes=i
+                    break
+            for i in range(0,len(prob_no)):
+                if(prob_yes[i]!=0):
+                    prob_no_fin=prob_no[i]
+                    avoid_no=i
+                    break
+            for i in range(1,len(prob_yes)):
+                if(prob_yes[i]!=0 and avoid_yes != i):
+                    temp=prob_yes[i]
+                    if(prob_yes_fin*temp!=0):
+                        prob_yes_fin=prob_yes_fin*temp
+                    else: 
+                        break
+                if(prob_no[i]!=0 and avoid_no != i):
+                    temp=prob_no[i]
+                    if(prob_no_fin*temp!=0):
+                        prob_no_fin=prob_no_fin*temp
+                    else: 
+                        break
+                if(i==11 and prob_yes_fin > prob_no_fin):
+                    classified.append(1)
+                if(i==11 and prob_no_fin >= prob_yes_fin):
+                    classified.append(0)
+        return classified, test_disc_50k
+    def error(test_disc_50k, classified_50k): # Returns percent error
+        INST=16281
+        num_errors=0
+        for i,j in zip(test_disc_50k,classified_50k):
+            if i!=j: num_errors+=1
+        return (num_errors/INST)*100 # Return percent error
+    # Get list of all probabilities
+    P_GT50K_1,P_LT50K_1,P_GT50K_0,P_LT50K_0=gen_list_prob(LABELS,ad_df)
+    # Get list of classified values from test and actual results
+    classified, test_disc_50k=test(LABELS,test_df,P_GT50K_1,P_LT50K_1,P_GT50K_0,P_LT50K_0)
+    # Compute percent error
+    errors=error(test_disc_50k, classified)
+    # Display percent error
+    print(f"Percent error: %{errors:.2f}")
+    
 # Program begins here
+def main():
+    # Names of files we will read from
+    ADULT_DATA = 'adult.data'
+    TEST_DATA = 'adult.test'
+    # Labels associated with data
+    LABELS = ['age','workclass','fnlwgt','education','education_num',
+            'marital_status','occupation','relationship','race',
+            'sex','capital_gain','capital_loss','hours_per_week',
+            'native_country','_50k']
+    # Get data from file
+    ad_df = store_data_from_file(ADULT_DATA, LABELS)   
+    test_df = store_data_from_file(TEST_DATA, LABELS)
+    # Naive bayes implementation
+    naive_bayes(ad_df, test_df, LABELS)
 
-# Names of files we will read from
-ADULT_DATA = 'adult.data'
-TEST_DATA = 'adult.test'
-# Labels associated with data
-LABELS = ['age','workclass','fnlwgt','education','education_num',
-          'marital_status','occupation','relationship','race',
-          'sex','capital_gain','capital_loss','hours_per_week',
-          'native_country','_50k']
-# Get data from file
-ad_df = store_data_from_file(ADULT_DATA, LABELS)   
-test_df = store_data_from_file(TEST_DATA, LABELS)
-# Naive bayes implementation
-naive_bayes(ad_df, test_df, LABELS)
+if __name__ == "__main__":
+    main()
